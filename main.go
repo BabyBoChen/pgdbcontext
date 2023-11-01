18bf287949c30df63912f4dd4f6a21836287bb98
BabyBo <35010886+BabyBoChen@users.noreply.github.com>
2023-11-01 16:46:10 +0800

    Update main.go

--- main.go
+++ main.go
@@ -15,34 +15,42 @@ func main() {
 		repo, err = db.GetRepository("dish")
 	}
 
-	var lastInsertedId map[string]interface{}
 	if err == nil {
-		cellValues := make(map[string]interface{})
-		cellValues["title"] = "鮭魚壽司"
-		cellValues["unit_price"] = 25.00
-		cellValues["row_order"] = 13
-		lastInsertedId, err = repo.Insert(cellValues)
-	}
-
-	if err == nil {
+		toUpdate := make(map[string]interface{})
+		toUpdate["dish_id"] = "e90ba433-4181-4945-b1e5-3d2235bfef9d"
+		toUpdate["unit_price"] = 75
+		err = repo.Update(toUpdate)
 		db.Commit()
-		fmt.Println(lastInsertedId)
 	}
 
-	// var dt *DataTable
+	// var lastInsertedId map[string]interface{}
 	// if err == nil {
-	// 	dt, err = repo.Select(" dish_id=$1 ", "c9313e1b-4f03-431d-8987-5a96aa050fad")
+	// 	cellValues := make(map[string]interface{})
+	// 	cellValues["title"] = "鮭魚壽司"
+	// 	cellValues["unit_price"] = 25.00
+	// 	cellValues["row_order"] = 13
+	// 	lastInsertedId, err = repo.Insert(cellValues)
 	// }
 
 	// if err == nil {
-	// 	for i := 0; i < len(*dt.Rows); i++ {
-	// 		row := (*dt.Rows)[i]
-	// 		for j := 0; j < len(*row.Cells); j++ {
-	// 			cell := (*row.Cells)[j]
-	// 			fmt.Println(cell.Value(nil))
-	// 		}
-	// 	}
+	// 	db.Commit()
+	// 	fmt.Println(lastInsertedId)
 	// }
+
+	var dt *DataTable
+	if err == nil {
+		dt, err = repo.Select("",)
+	}
+
+	if err == nil {
+		for i := 0; i < len(*dt.Rows); i++ {
+			row := (*dt.Rows)[i]
+			for j := 0; j < len(*row.Cells); j++ {
+				cell := (*row.Cells)[j]
+				fmt.Println(cell.GetValue())
+			}
+		}
+	}
 
 	if err != nil {
 		fmt.Println(err)
